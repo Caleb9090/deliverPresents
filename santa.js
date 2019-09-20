1d0ce99bb535501af12c45d8e5e4d79c57a579c9
Caleb F <calebsf2000@gmail.com>
2019-09-20 12:34:08 -0400

    used-slice-and-var-to-make-elves-deliver-when-they-had-one-house

--- santa.js
+++ santa.js
@@ -1,5 +1,16 @@
 function deliverPresents(houses) {
-  console.log('Delivering presents to ' + houses[0])
+  if (houses.length === 0) return
+  
+  if (houses.length === 1) {
+    console.log('Delivering presents to ' + houses[0])
+  } else {
+    var middle = parseInt(houses.length / 2)
+    var firstHalf = houses.slice(0, middle)
+    var secondHalf = houses.slice(middle)
+
+    deliverPresents(firstHalf)
+    deliverPresents(secondHalf)
+  }
 }
 
 module.exports = deliverPresents
